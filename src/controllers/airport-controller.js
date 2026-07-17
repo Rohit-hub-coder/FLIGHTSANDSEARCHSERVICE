369de0d10d47c62426a2f9feaef19fb9b5a5a6e6
Rohit-hub-coder <149248035+RohitttK@users.noreply.github.com>
2026-07-18 02:49:52 +0530

    refactor: migrate Airport module to use CrudRepository/CrudService inheritance

--- src/controllers/airport-controller.js
+++ src/controllers/airport-controller.js
@@ -3,7 +3,7 @@ const airportService = new AirportService();
 
 const create = async (req, res) => {
     try {
-        const airport = await airportService.createAirport(req.body);
+        const airport = await airportService.create(req.body);  // pehle: createAirport
         return res.status(201).json({
             success: true,
             message: "Successfully created an airport",
@@ -23,7 +23,7 @@ const create = async (req, res) => {
 const get = async (req, res) => {
     try {
         const airportId = req.params.id;
-        const airport = await airportService.getAirport(airportId);
+        const airport = await airportService.get(airportId);  // pehle: getAirport
         return res.status(200).json({
             success: true,
             message: "Successfully fetched an airport",
@@ -43,7 +43,7 @@ const get = async (req, res) => {
 const destroy = async (req, res) => {
     try {
         const airportId = req.params.id;
-        await airportService.deleteAirport(airportId);
+        await airportService.destroy(airportId);  // pehle: deleteAirport
         return res.status(200).json({
             success: true,
             message: "Successfully deleted the airport",
@@ -63,7 +63,7 @@ const destroy = async (req, res) => {
 const update = async (req, res) => {
     try {
         const airportId = req.params.id;
-        const airport = await airportService.updateAirport(airportId, req.body);
+        const airport = await airportService.update(airportId, req.body);  // pehle: updateAirport
         return res.status(200).json({
             success: true,
             message: "Successfully updated the airport",
--- src/repository/airport-repo.js
+++ src/repository/airport-repo.js
@@ -1,50 +1,9 @@
+const CrudRepository = require('./crud-repo');
 const { Airport } = require('../models/index');
 
-class AirportRepository {
-
-    async createAirport(data) {
-        try {
-            const airport = await Airport.create(data);
-            return airport;
-        } catch (error) {
-            console.log("Something went wrong in the repository layer");
-            throw {error};
-        }
-    }
-
-    async getAirport(airportId) {
-        try {
-            const airport = await Airport.findByPk(airportId);
-            return airport;
-        } catch (error) {
-            console.log("Something went wrong in the repository layer");
-            throw {error};
-        }
-    }
-
-    async destroyAirport(airportId) {
-        try {
-            await Airport.destroy({
-                where: { id: airportId }
-            });
-            return true;
-        } catch (error) {
-            console.log("Something went wrong in the repository layer");
-            throw {error};
-        }
-    }
-
-    async updateAirport(airportId, data) {
-        try {
-            const airport = await Airport.findByPk(airportId);
-            airport.name = data.name;
-            airport.address = data.address;
-            await airport.save();
-            return airport;
-        } catch (error) {
-            console.log("Something went wrong in the repository layer");
-            throw {error};
-        }
+class AirportRepository extends CrudRepository {
+    constructor() {
+        super(Airport);
     }
 }
 
